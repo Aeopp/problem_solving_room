b6a78ba3aaa0eebe5e887630f7efb6b8e8bf5876
HelloEnavy <gusdk1793@gmail.com>
2022-01-28 15:14:59 +0900

    함수 - 정수 N개의 합

---
+++ baekjoon8.java
@@ -0,0 +1,12 @@
+public class baekjoon8 {
+    public long sum (int[] a) {
+      long sum = 0;
+      for(int i=0 ; i<a.length ; i++) {
+          sum += a[i];
+      }      
+      return sum;
+    }
+}
+
+
+
