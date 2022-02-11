33672119c94c0cabc9621568739c0e9d30f6cf21
HelloEnavy <gusdk1793@gmail.com>
2022-02-11 16:24:20 +0900

    0211 - 문자열 아스키코드

---
+++ BAEKJOON/현아/baekjoon11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class baekjoon11 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        char answer = scanner.nextLine().charAt(0);
+        int number = (int)answer;
+
+        System.out.println(number);
+
+    }
+}
