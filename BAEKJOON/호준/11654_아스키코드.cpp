9dffbfc60a9f5a6594fa710d64d5d59aea3ba698
DESKTOP-5F2M5SL\이호준 <irmxj777@gmail.com>
2022-02-14 22:57:31 +0900

    - 백준 11654 아스키 코드 (https://www.acmicpc.net/problem/11654)

---
+++ BAEKJOON/호준/11654_아스키코드.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <regex>
+#include <string>
+#include <sstream>
+#include <string_view>
+#include <array>
+#include <tuple>
+#include <set>
+#include <numeric>
+#include <algorithm>
+#include <numbers>
+#include <map>
+
+int main()
+{
+	char c;
+	std::cin >> c;
+	std::cout << static_cast<int>(c);
+
+
+	return 0;
+};
