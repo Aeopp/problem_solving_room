06e3ccee4a38fc71cc80087ed0dcf51bcec4aff4
HelloEnavy <gusdk1793@gmail.com>
2022-04-20 23:49:54 +0900

    0420 문자열 - 그룹 단어 체커

---
+++ BAEKJOON/현아/baekjoon20.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class baekjoon21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int word = scanner.nextInt();
+        String[] wordArr = new String[word];
+        int count = 0;
+
+        for(int i=0 ; i<word ; i++) {
+            wordArr[i] = scanner.next();
+        }
+
+        for(int i=0 ; i<wordArr.length ; i++) {
+            
+            List<String> wordList = new ArrayList<String>();
+
+            
+            
+            boolean check1 = true;
+
+            for(int z=0 ; z<wordArr[i].length() ; z++) {
+                boolean check = true;
+                
+                List<Integer> indexArr = new ArrayList<>();
+                String a = wordArr[i].charAt(z)+"";
+
+                for(int j=0 ; j<wordList.size() ; j++) {
+                    if(a.equals(wordList.get(j))){
+                        check = false;
+                    }
+                }
+
+                if(check) {
+                    for(int j=0 ; j<wordArr[i].length() ; j++) {
+
+                        if(a.equals(wordArr[i].charAt(j)+"")) {
+                            indexArr.add(j);
+                        }
+                    }
+                    wordList.add(a);
+                }
+
+                System.out.println("a : " + a);
+
+                for(int p=0 ; p<indexArr.size() ; p++) {
+                    System.out.println("indexArr["+p+"]" +indexArr.get(p));
+                }
+
+                for(int j=0 ; j<indexArr.size()-1 ; j++) {
+                    if(indexArr.get(j+1) - indexArr.get(j) > 1) {
+                        check1 = false;
+                    }
+                }
+            }
+
+            if(check1) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+        scanner.close();
+    }
+    
+}
