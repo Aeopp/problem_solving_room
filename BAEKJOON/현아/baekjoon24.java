6f5d62ce4d471df7c0ee11473fdeecabb8fcd846
HelloEnavy <gusdk1793@gmail.com>
2022-04-29 15:11:31 +0900

    no message

--- BAEKJOON/현아/baekjoon24.java
+++ BAEKJOON/현아/baekjoon24.java
@@ -3,10 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class baekjoon23 {
+public class baekjoon24 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");s
 
         int go = Integer.parseInt(st.nextToken());
         int sleep = Integer.parseInt(st.nextToken());
